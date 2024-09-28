7271005df896e3bca95a0e9a431f3e09ab981850
blwatkins <blwatkins@users.noreply.github.com>
2024-09-27 19:05:39 -0500

    Add lines that utilize default stroke.

--- src/sketch.ts
+++ src/sketch.ts
@@ -17,14 +17,14 @@
 
 import P5Lib from 'p5';
 
-import {ASPECT_RATIOS, CanvasContext, Color, SketchContext} from '@batpb/genart';
+import { ASPECT_RATIOS, CanvasContext, Color, PC_7A00F5, SketchContext } from '@batpb/genart';
 
 import '../assets/styles/sketch.css';
 
 function sketch(p5: P5Lib): void {
     p5.setup = (): void => {
         SketchContext.initialize(p5);
-        CanvasContext.buildCanvas(ASPECT_RATIOS.SQUARE, 720, true, p5.WEBGL);
+        CanvasContext.buildCanvas(ASPECT_RATIOS.SQUARE, 720);
     };
 
     p5.draw = (): void => {
@@ -33,11 +33,26 @@ function sketch(p5: P5Lib): void {
 
         const colorA: Color = new Color(p5.color(255, 0, 255));
         p5.fill(colorA.color);
-        p5.rect(0, 0, 250, 250);
+        p5.rect(CanvasContext.mapXPercentage(0.5), 0, 250, 250);
 
         const colorB: Color = new Color(p5.color(0, 0, 255));
         p5.fill(colorB.color);
-        p5.rect(0, 0, 75, 75);
+        p5.rect(CanvasContext.mapXPercentage(0.5), 0, 75, 75);
+
+        p5.stroke((new Color(PC_7A00F5)).color);
+        p5.strokeWeight(CanvasContext.defaultStroke);
+        p5.line(
+            CanvasContext.mapXPercentage(0.1),
+            CanvasContext.minHeight,
+            CanvasContext.mapXPercentage(0.1),
+            CanvasContext.maxHeight)
+        ;
+        p5.line(
+            CanvasContext.mapXPercentage(0.9),
+            CanvasContext.minHeight,
+            CanvasContext.mapXPercentage(0.9),
+            CanvasContext.maxHeight
+        );
     };
 
     p5.keyPressed = (): void => {
